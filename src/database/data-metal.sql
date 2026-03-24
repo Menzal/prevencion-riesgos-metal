92567fa272277a78b12f760a79b2b49f8995f18a
Menzal <elvirahflores@gmail.com>
2026-03-24 22:10:58 +0100

    Create metal sector hazards data file with 10 hazards

---
+++ src/database/data-metal.sql
@@ -0,0 +1,13 @@
+-- Metal Sector Hazards Data
+
+INSERT INTO hazards (hazard, location) VALUES
+('caídas de altura', 'Murcia'),
+('contacto eléctrico', 'Murcia'),
+('quemaduras', 'Murcia'),
+('lesiones por maquinaria', 'Murcia'),
+('proyección de partículas', 'Murcia'),
+('inhalación de humos', 'Murcia'),
+('ruido', 'Murcia'),
+('vibraciones', 'Murcia'),
+('estrés térmico', 'Murcia'),
+('lesiones de espalda', 'Murcia');
